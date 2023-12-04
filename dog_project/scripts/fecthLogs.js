a59e55da74440d3e0667703d1df4c3516f584763
Gabriel Teodoro <teodorogabriel367@gmail.com>
2023-12-04 16:57:38 -0300

    feat:fecth logs done

---
+++ dog_project/scripts/fecthLogs.js
@@ -0,0 +1,27 @@
+﻿export default async function fetchLogs(setLogs, setError) {
+    try {
+      const response = await fetch("https://eokelvio.pythonanywhere.com/log/");
+  
+      if (response.ok) {
+        const json = await response.json();
+        if (Array.isArray(json)) {
+          const extractedLogs = json.map((item) => ({
+            id: item.id,
+            food_liberation: item.food_liberation,
+            portions: item.portions,
+            food_level: item.food_level,
+            created_at: item.created_at,
+          }));
+          
+          setLogs(extractedLogs);
+        } else {
+          setError(true);
+        }
+      } else {
+        setError(true);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  
