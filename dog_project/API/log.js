7ed79c743c4c570443af907a20e19ced4d2fa60c
Gabriel Teodoro <teodorogabriel367@gmail.com>
2023-12-07 20:09:43 -0300

    post log function

---
+++ dog_project/API/log.js
@@ -0,0 +1,28 @@
+﻿export async function postHour(foodLiberation, portions, foodLevel) {
+    const link = `https://eokelvio.pythonanywhere.com/log/`;
+    try {
+      const data = {
+        food_liberation: foodLiberation,
+        portions: portions,
+        food_level: foodLevel,
+      };
+  
+      const response = await fetch(link, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+  
+      if (response.ok) {
+        const createHours = await response.json();
+        console.log("Horário criado:", createHours);
+      } else {
+        console.error("Erro ao criar o horário:", response.statusText);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  
